af41c4fa32e1477483fa77b74042c083f9fc691b
Omer Mohamed Mahmoud <100491746+LinkBroken@users.noreply.github.com>
2024-04-27 23:25:16 +0300

    Update navToggle.js

--- static/navToggle.js
+++ static/navToggle.js
@@ -1,18 +1,23 @@
 const navBar = document.getElementsByClassName("nav-bar");
 const removeButton = document.getElementById("remove-nav");
 const showButton = document.getElementById("show-nav");
+showButton.style.display= "none";
 
 showButton.addEventListener("click", ()=>{
-    localStorage.clear()
+    localStorage.clear();
+    showButton.style.display = "none";
+    removeButton.style.display = "block";
     navBar[0].style.display = "flex";
     
 });
 
 removeButton.addEventListener("click", ()=>{
     navBar[0].style.display = "none";
+    removeButton.style.display = "none";
+    showButton.style.display = "block";
     localStorage.setItem("remove-navbar", "none");
 });
 
 if (localStorage.length > 0){
-    navBar[0].style.display = localStorage.getItem("remove-navbar")
+    navBar[0].style.display = localStorage.getItem("remove-navbar");
 };
